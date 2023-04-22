2f4718ff9b92ed29072fb092f2bf6d6b7ebda6db
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-04-22 09:53:03 -0500

    tah id is joiining the table between tag and product

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -11,17 +11,24 @@ Product.belongsTo(Category, {
 })
 
 // Categories have many Products
+
 Category.hasMany(Product, {
   foreignKey: 'category_id'
 })
 
 // Products belongToMany Tags (through ProductTag)
+
 Product.belongsToMany(Tag, {
   foreignKey: 'product_id', 
   through: ProductTag
 })
 
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  foreignKey: 'tag_id', //originally product_id
+  through: ProductTag
+})
 
 module.exports = {
   Product,
