fba316b423c8410f40b0370f10094d91811569c0
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-04-22 09:59:38 -0500

    adding a tag id

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -21,7 +21,15 @@ ProductTag.init(
       }
     },
 
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
+
   {
     sequelize,
     timestamps: false,
