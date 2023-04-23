b104611dc326b3a89669b3239c0ee5dd6d64712b
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-04-23 14:23:51 -0500

    connecting index js with connections js

--- Develop/routes/api/index.js
+++ Develop/routes/api/index.js
@@ -3,6 +3,9 @@ const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
 
+
+const sequelize = require('../../config/connection');
+
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
