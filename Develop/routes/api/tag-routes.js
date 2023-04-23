2e9d5012ae27688297c2136d27df2b18fad9afe9
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-04-23 14:32:23 -0500

    adding a const tags

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -14,13 +14,26 @@ router.get('/', async (req, res) => {
 
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  const tag = await Tag.findOne({
+    where: {id: req.params.id},
+    include: [Product]
+  })
+  res.status(200).json(tag)
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tag = await Tag.create(req.body)
+    res.status(200).json(tag)
+    } catch(err) {
+      console.log(err);
+      res.status(400).json(err);
+    }
+
 });
 
 router.put('/:id', (req, res) => {
