004fc824644b003007d2f9a9b9a56a0d92ba7aec
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-04-23 14:25:23 -0500

    adding a const product

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -10,9 +10,14 @@ router.get('/', (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  const product = await Product.findOne({
+    where: {id: req.params.id},
+    include: [Category, {model: Tag, through: ProductTag}] 
+  })
+  res.status(200).json(product)
 });
 
 // create new product
