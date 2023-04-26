c196622ed2c7f88c64db2cc78863ff911a5fee72
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-04-25 19:55:38 -0500

    adding comments on thecategory seeds file

--- Develop/seeds/category-seeds.js
+++ Develop/seeds/category-seeds.js
@@ -1,3 +1,10 @@
+//-- Adding a function seedCategories() that seeds the Category model with some initial data--//
+//-- Adding categoryData array contains objects that represent the data to be inserted into the Category table--//
+//-- Adding seedCategories() function calls the bulkCreate() method on the Category model, which inserts multiple rows into the Category--//
+//-- And Finally seedCategories() function is exported, which allows it to be imported and called by other files--//
+
+
+
 const { Category } = require('../models');
 
 const categoryData = [
