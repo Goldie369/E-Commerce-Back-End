27add4ce3321f5d5c8999c4bf0eb304b8ac5099a
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-04-25 19:52:39 -0500

    adding more comments

--- Develop/server.js
+++ Develop/server.js
@@ -11,6 +11,10 @@ const connection = require("./config/connection")
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+//-- Adding express.json() and express.urlencoded() functions are used to set up middleware for parsing incoming requests that have a JSON or URL-encoded body--//
+//-- Adding app.use(routes) to define the endpoints and corresponding handlers for the server--//
+//-- Adding connection.sync() method is called to synchronize the database models--//
+//-- Adding app.listen() method, which binds to the specified port and starts accepting incoming requests--//
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
