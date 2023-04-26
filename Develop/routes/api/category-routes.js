bfd4f7d028291eefdcefae2bef6b81a4c047bac7
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-04-25 20:48:21 -0500

    modifying the category routes to make it work

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -21,6 +21,11 @@ router.get('/', async(req, res) => {
   // be sure to include its associated Products
 });
 
+
+//-- Adding a GET requests to the /api/categories/:id endpoint--//
+//-- the code uses Sequelize to query the Category model and retrieve the category with the corresponding id from the database--//
+//-- JSON.stringify() method and sends the response back to the user with res.send()--//
+
 router.get('/:id', async(req, res) => {
   // find one category by its `id` value
   const categories=await Category.findByPk(req.params.id)
@@ -28,6 +33,9 @@ router.get('/:id', async(req, res) => {
   res.send(result)
   // be sure to include its associated Products
 });
+
+
+//-- Same comment like the one above just this is for the POST route--//
 
 router.post('/', async(req, res) => {
   console.log(req.body)
@@ -40,10 +48,47 @@ router.post('/', async(req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  router.put('/:id', async (req, res) => {
+  try {
+    // find the category by id
+    const category = await Category.findByPk(req.params.id);
+
+    if (!category) {
+      return res.status(404).send('Category not found');
+    }
+
+    // update the category with the new data
+    const updatedCategory = await category.update(req.body);
+
+    // send the updated category back to the client
+    res.json(updatedCategory);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error');
+  }
 });
 
-router.delete('/:id', (req, res) => {
+});
+
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    // find the category by id
+    const category = await Category.findByPk(req.params.id);
+
+    if (!category) {
+      return res.status(404).send('Category not found');
+    }
+
+    // delete the category
+    await category.destroy();
+
+    // send a success response back to the client
+    res.send('Category deleted successfully');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error');
+  }
 });
 
 module.exports = router;
